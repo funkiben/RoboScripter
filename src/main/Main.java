cbc52fbc08b0f5805418ef118066ab1f7e3fd203
funkiben <funkiben@gmail.com>
2015-10-28 14:57:38 -0400

    Example script

--- src/main/Main.java
+++ src/main/Main.java
@@ -37,9 +37,9 @@ public class Main {
 		script.addInstruction(new TestInstruction(script, cx, cy));
 		script.addInstruction(new TestInstruction(script, cx, cy));
 		
-		//scripter.getWindow().setScript(script);
+		scripter.getScriptDB().save(script);
 		
-		//scripter.getScriptDB().save(script);
+		scripter.getWindow().getScriptDBFrame().updateList();
 		
 	}
 
